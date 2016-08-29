053d36599a3bdaf9479b6f7f8a78e7222dd82894
sleeper_qp <qipeng1922@gmail.com>
2016-08-30 00:12:48 +0800

    softmax回归：测试不同梯度下降的优化方法，PS调参真是玄学~

--- model/SoftmaxRegression.py
+++ model/SoftmaxRegression.py
@@ -1,5 +1,8 @@
+#coding=utf-8
 from time import time
 
+import matplotlib.pyplot as plt
+import pylab as pl
 import numpy as np
 import math
 from sklearn.datasets import fetch_mldata
@@ -9,6 +12,9 @@ from sklearn.cross_validation import train_test_split
 class SR(object):
     def __init__(self, input_size, output_size):
         self.w = np.random.randn(input_size + 1, output_size) / math.sqrt(input_size + 1)
+        self.pre_w = np.zeros(shape=(input_size + 1, output_size))
+        self.w_cache =  np.zeros(shape=(input_size + 1, output_size))
+        self.eps = 1e-9
         # self.w = np.zeros(shape=(input_size + 1, output_size))
         self.input_size = input_size
         self.output_size = output_size
@@ -21,37 +27,57 @@ class SR(object):
         e_x = np.exp(x - np.max(x))
         return e_x / e_x.sum(axis=0)  # only difference
 
-    def update(self, data, label):
+    def update(self, data, label, gama=0.2):
         # wx+b: batch x input * input x output = batch x output
-        # print 'data:', data
         output = np.dot(data, self.w)
-        # print output
-        # print 'output', output
+        # ps : batch x output
         ps = self.softmax(output)
-        # exp_output = np.exp(output- np.max(output)/2)
-        # print 'exe_output:', exp_output
-        # print exp_output.shape
-        # print (np.tile(np.sum(exp_output, axis=1), self.output_size).reshape(len(data), self.output_size)).shape
-        # ps = exp_output / (np.tile(np.sum(exp_output, axis=1), self.output_size).reshape(len(data), self.output_size))
         for idx in range(0, len(ps)):
             ps[idx][int(label[idx])] -= 1.0
-        # ps : batch x output
-        #  intput x batch * batch x output = input x output
-        # print 'ps:', ps
+
         delta = (1.0 / len(data)) * np.dot(data.T, ps)
         # print 'delta:', delta
-        self.w -= self.alpha * delta + 0.05 * self.w
+        # 比较不同的GD优化方法
+        # sgd： 0.292571428571
+        # Delta = self.alpha * delta + 0.3 * self.w
+
+        # adagrad
+        self.w_cache = self.w_cache + delta**2
+        Delta = self.alpha * delta/(np.sqrt(self.w_cache)+self.eps)
+        # Momentum:
+        # Delta = gama * self.pre_w + self.alpha * delta
+        # self.pre_w = Delta
+
+
+        self.w -= Delta
 
         # print 'w:', self.w
         pass
 
-    def train(self, datas, labels, batch_num=200, alpha=0.03, iterats=500, stop_condition=1e-5):
+    def Nesterov_update(self, data, label, gama=0.9):
+        w = self.w - gama*self.pre_w
+        output = np.dot(data, w)
+        ps = self.softmax(output)
+        for idx in range(0, len(ps)):
+            ps[idx][int(label[idx])] -= 1.0
+        delta = (1.0 / len(data)) * np.dot(data.T, ps)
+
+        # Nesterov
+        Delta = gama * self.pre_w + self.alpha * delta
+        self.pre_w = Delta
+        self.w -= Delta
+
+
+    def train(self, datas, labels, X_test, y_test, batch_num=100, alpha=0.1, iterats=25, stop_condition=1e-5):
         self.batch = batch_num
         self.alpha = alpha
 
         datas = np.c_[datas, np.ones(len(datas))]
         iter_num = (len(datas) + batch_num - 1) / batch_num
 
+        xs = []
+        acc = []
+        idx = 0
         for i in range(0, iterats):
             print 'iter:', i
             for j in range(0, iter_num):
@@ -60,6 +86,12 @@ class SR(object):
                 data = datas[start: end]
                 label = labels[start: end]
                 self.update(data, label)
+                # self.Nesterov_update(data, label)
+                xs.append(idx)
+                acc.append(self.test(X_test, y_test, o='no_output'))
+                idx += 1
+        pl.plot(range(0, len(acc)), acc)  # use pylab to plot x and y
+        pl.show()
         pass
 
     def predict(self, data):
@@ -71,17 +103,16 @@ class SR(object):
 
         return np.argmax(ps, axis=1)
 
-    def test(self, datas, label):
+    def test(self, datas, label, o='output'):
         predict_label = self.predict(datas)
-        print predict_label
+        # print predict_label
         error = 0
-        print type(predict_label)
-        print type(label)
         for idx in range(0, len(label)):
             if (int(predict_label[idx]) != int(label[idx])):
                 error += 1
-        print 'error:', error / float(len(datas))
-
+        if o=='output':
+            print 'error:', error / float(len(datas))
+        return error / float(len(datas))
 
 if __name__ == '__main__':
     sr = SR(784, 10)
@@ -97,10 +128,11 @@ if __name__ == '__main__':
                 datas[idx][j] = 1
     labels = mnist.target
 
-    X_train, X_test, y_train, y_test = train_test_split(datas, labels, test_size=0.33, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(datas, labels, test_size=0.05, random_state=42)
     print 'starting train.....'
     now = time()
-    sr.train(X_train, y_train)
+    sr.train(X_train, y_train, X_test, y_test)
     train_time = time() - now
     print 'finish train :', train_time, 's'
     sr.test(X_test, y_test)
+    sr.test(X_train, y_train)
